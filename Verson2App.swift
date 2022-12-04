d178e0b32dabc1b58c4c4ec8fc143c4e52b09121
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-03 20:10:52 -0600

    Version 2

---
+++ Verson2App.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                ContentView()
+                    .tabItem {
+                        Label("Journal", systemImage: "book")
+                    }
+                SettingsView()
+                    .tabItem {
+                        Label("Settings", systemImage: "gear")
+                        
+                    }
+            }
+        }
+    }
+}
